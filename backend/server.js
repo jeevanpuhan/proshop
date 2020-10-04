ad4e9e3a4d51a931518261ebf4f6b760efbaa169
Jeevan Puhan <jeevankumarpuhan@gmail.com>
2020-10-04 16:01:44 +0530

    backend initial setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`Server running on port: ${port}`));
